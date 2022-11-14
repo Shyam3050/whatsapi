af62217682c2e73226b418e6a87ab6ff8d1fd1fd
Shyam3050 <nimmanashyam3050@gmail.com>
2022-11-14 19:16:08 +0530

    U

--- script.js
+++ script.js
@@ -1,20 +1,11 @@
-const chat = document.getElementById("chat");
-const number = document.getElementById("numberInp");
+const wts = document.getElementById("wts");
+const form_inp = document.getElementById("form_inp");
 
-chat.addEventListener("click", chatSub);
-function chatSub(e) {
-  if (number.value.length < 10) {
-    alert("Check it", number.value);
+wts.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (form_inp.value.length < 10) {
+    alert("Check it");
     return;
   }
-  chat.href = `https://wa.me/+91${number.value}`;
-}
-
-const wts = document.getElementById('wts')
-const form_inp = document.getElementById('form_inp');
-
-console.log(form_inp.value);
-wts.addEventListener('submit' , e=>{
-  e.preventDefault()
-  location.href = `https://wa.me/+91${form_inp.value}`
-})
+  location.href = `https://wa.me/+91${form_inp.value}`;
+});
